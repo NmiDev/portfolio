ab587d26ce53de32b97783a4b0630c5ec6ed48bc
NmiDev <missiaen.nicolas@gmail.com>
2019-06-14 11:31:27 +0200

    Update store with devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,24 @@
+/*
+ * Npm import
+ */
 import { createStore } from 'redux';
 
+/*
+ * Local import
+ */
+// Reducer
 import reducer from './reducer';
 
-const store = createStore(reducer);
+/*
+ * Code
+ */
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
+/*
+ * Export
+ */
 export default store;
